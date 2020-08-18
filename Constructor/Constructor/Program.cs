4083ed2378f44e356e6d32ba7e8f2c9255fb0bea
wohoo21cn <43713497+wohoo21cn@users.noreply.github.com>
2020-08-18 10:12:43 +0800

    Initial commit

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,50 @@
+﻿using Constructor;
+using System;
+
+namespace Constructor
+{
+   class myClass
+    {
+        readonly int firstVar;
+        readonly double secondVar;
+
+        public string userName;
+        public int userIdNumber;
+
+        private myClass()
+        {
+            firstVar = 20;
+            secondVar = 30;
+
+        }
+
+        public myClass(string firstName):this()
+        {
+            userName = firstName;
+            userIdNumber = -1;
+        }
+
+        public myClass(int idNumber) : this()
+        {
+            userName = "Anonymous";
+            userIdNumber = idNumber;
+        }
+    }
+    
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        myClass mc = new myClass("Zhangsan");
+        myClass mc2 = new myClass(2);
+        Console.WriteLine($"myClass : {mc.userName },{mc.userIdNumber}");
+        Console.WriteLine($"myClass : {mc2.userName },{mc2.userIdNumber}");
+        Console.WriteLine($"myClass : {mc2.firstVar},{mc2.userIdNumber}");
+        Console.ReadKey();
+
+
+        }
+    }
+
---
+++ static properties/static properties/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static static_properties.Trivial;
+
+namespace static_properties
+{
+    class Trivial
+    {
+        public static int MyValue { get; set; }
+        public void PrintValue()
+        {
+            Console.WriteLine("Value from inside {0}",MyValue  );
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Init Value {0}",Trivial .MyValue    );
+            Trivial.MyValue = 10;
+            Console.WriteLine("Init Value {0}", Trivial.MyValue);
+
+            MyValue = 20;
+            Console.WriteLine($"new value: {MyValue} ");
+
+            Console.ReadKey();
+        }
+    }
+}
